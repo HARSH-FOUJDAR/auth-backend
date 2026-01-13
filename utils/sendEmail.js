aa3ed410bb4770840eb35d519f64fafa5378d177
Harsh Foujdar <harshbtp738@gmail.com>
2026-01-13 16:03:28 +0530

    update

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,24 +1,18 @@
-const nodemailer = require("nodemailer");
+const sgMail = require("@sendgrid/mail");
 const resetPasswordEmailTemplate = require("./otpEmailTemplate");
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const sendEmail = async (email, resetLink) => {
   try {
-    const transporter = nodemailer.createTransport({
-      host: "smtp.sendgrid.net",
-      port: 587,
-      auth: {
-        user: "apikey", // fixed
-        pass: process.env.SENDGRID_API_KEY, // Render ENV
-      },
-    });
-
-    await transporter.sendMail({
-      from: `"Harsh Foujdar" <codingwithharshfoujdar@gmail.com>`,
+    const msg = {
       to: email,
+      from: "codingwithharshfoujdar@gmail.com", // verified sender in SendGrid
       subject: "Reset Your Password",
       html: resetPasswordEmailTemplate(resetLink, "Harsh Foujdar"),
-    });
+    };
 
+    await sgMail.send(msg);
     console.log("✅ Reset link email sent successfully");
   } catch (error) {
     console.log("❌ Email sending error:", error.message);
