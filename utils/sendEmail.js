bf2bdbc6585db4e5f97d54444a034d45305e54e6
Harsh Foujdar <harshbtp738@gmail.com>
2026-01-14 23:23:12 +0530

    dd

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -3,26 +3,23 @@ const resetPasswordEmailTemplate = require("./otpEmailTemplate");
 
 const sendEmail = async (email, resetLink) => {
   try {
-    // 1️⃣ Transporter (Gmail)
     const transporter = nodemailer.createTransport({
       service: "gmail",
       auth: {
-        user: process.env.EMAIL,      // your gmail id
-        pass: process.env.EMAIL_PASS // Gmail App Password
+        user: process.env.EMAIL,
+        pass: process.env.EMAIL_PASS,
       },
     });
 
-    // 2️⃣ Send mail
     await transporter.sendMail({
       from: `"Harsh Foujdar" <${process.env.EMAIL}>`,
       to: email,
       subject: "Reset Your Password",
       html: resetPasswordEmailTemplate(resetLink, "Harsh Foujdar"),
     });
-
-    console.log("✅ Email sent successfully");
+    console.log(" Email sent successfully");
   } catch (error) {
-    console.error("❌ Email sending error:", error.message);
+    console.error("Email sending error:", error.message);
   }
 };
 
