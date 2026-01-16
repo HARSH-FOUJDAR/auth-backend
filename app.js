ffa269a4da8b2f390001d5f87a3863976f99ca83
Harsh Foujdar <harshbtp738@gmail.com>
2026-01-16 21:33:40 +0530

    update

--- app.js
+++ app.js
@@ -6,17 +6,18 @@ dotenv.config();
 const Database = require("./config/Databse");
 
 const app = express();
-app.use(express.json());
-
 app.use(cors({
   origin: [
     "http://localhost:5173",
     "https://frontend-auth-nhku.vercel.app"
   ],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 }));
 
-
+// Preflight fix (IMPORTANT)
+app.options("*", cors());
 
 
 app.use(express.urlencoded({ extended: true }));
