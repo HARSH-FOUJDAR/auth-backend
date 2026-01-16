2b6bd3d06ba395666fd5ec132b920ced0dc46731
Harsh Foujdar <harshbtp738@gmail.com>
2026-01-16 21:20:06 +0530

    sd

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "https://frontend-auth-nhku.vercel.app/",
+    origin: "https://frontend-auth-nhku.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
