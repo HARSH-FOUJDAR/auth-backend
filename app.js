2319e485e6c7dbb7fa13cc5a62b769e1df887501
Harsh Foujdar <harshbtp738@gmail.com>
2026-01-17 09:28:35 +0530

    update

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const mongoose = require("mongoose");
 const Database = require("./config/Databse");
 
 const app = express();
@@ -20,21 +19,6 @@ app.use(cors({
 }));
 
 
-
-
-app.use(async (req, res, next) => {
-  try {
-    if (!isConnected) {
-      await Database();
-      isConnected = true;
-      console.log("MongoDB Connected");
-    }
-    next();
-  } catch (error) {
-    console.error(" MongoDB Connection Error:", error.message);
-    res.status(500).json({ message: "Database connection failed" });
-  }
-});
 Database();
 app.get("/", (req, res) => {
   res.json({ message: "Server is running & CORS is working" });
